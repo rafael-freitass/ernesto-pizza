8b77f0153ffd5dd50e9953870a0d4eb78e80727e
undead-myauti <myauti.rafael@gmail.com>
2024-11-12 09:50:26 -0300

    Creates Menu Handler

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,18 @@
+import java.util.Scanner;
+
 import domain.Loader;
 
 public class Main{
     public static void main(String[] args){
+        @SuppressWarnings("resource")
+        Scanner sc = new Scanner(System.in);
         int opt = 0;
-        while(opt != 5){
+        while(opt != 3){
             Loader.LoadLogo();
-            System.out.println("\n")
-            Loader.LoadMenu()
+            System.out.println("\n");
+            Loader.LoadMenu();
+            opt = sc.nextInt();
+            Loader.MenuHandler(opt);
         }
     }
 }
--- src/domain/Loader.java
+++ src/domain/Loader.java
@@ -2,6 +2,11 @@ package domain;
 
 public class Loader {
 
+    public static void clearScreen() {  
+        System.out.print("\033[H\033[2J");  
+        System.out.flush();  
+    }
+
     public static void LoadLogo(){
         System.out.println("\n" +
                 "  ______                          _           _____  _                 \n" +
@@ -11,14 +16,33 @@ public class Loader {
                 " | |____ | |   | | | ||  __/\\__ \\| |_| (_) | | |    | | / /  / /| (_| |\n" +
                 " |______||_|   |_| |_| \\___||___/ \\__|\\___/  |_|    |_|/___|/___|\\__,_|\n" +
                 "                                                                       \n" +
-                "                                                                       \n");
+                "                                                                       ");
     }
 
     public static void LoadMenu(){
-        System.out.println("Realize seu pedido aqui:\n");
+        System.out.println("Realize seu pedido aqui:");
         System.out.println("-------------- ###### --------------");
         System.out.println("[1] Ver cardápio");
         System.out.println("[2] Realizar Pedido");
+        System.out.println("[3] Sair");
         System.out.println("-------------- ###### --------------");
+    }
+
+    public static void LoadPizza(){
+        Pizza p1 = new Pizza("Calabresa", "Calabresa", new String[] {"Mussarela"}, "M", 0)
+    }
+
+    public static void LoadBebida(){
+       
+    }
+
+    public static void LoadCardapio(){
+       
+    }
+
+    public static int MenuHandler(int opt){
+        clearScreen();
+
+        return 0;
     }
 }
